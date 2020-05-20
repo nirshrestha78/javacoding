3b496b92be06070de2766e4dd94b7f93cb9c56a4
nirshrestha78 <65616744+nirshrestha78@users.noreply.github.com>
2020-05-20 12:58:30 -0500

    laxmi sir pls download this latest update
    please use code and let me know

---
+++ laxmi/src/main/java/com/example/demo/LaxmiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaxmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaxmiApplication.class, args);
+	}
+
+}
---
+++ laxmi/src/main/java/laxmipck/Laxmiclass.java
@@ -0,0 +1,10 @@
+package laxmipck;
+
+public class Laxmiclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
